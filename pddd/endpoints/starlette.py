21dbaf14b037b2a8d5d397c2f78b627a6f981798
Xurvan <xurvan@tutanota.com>
2022-01-03 12:29:12 +0330

    Add "starlette" endpoints

---
+++ pddd/endpoints/starlette.py
@@ -0,0 +1,83 @@
+from starlette.endpoints import (
+    HTTPEndpoint,
+)
+from starlette.requests import (
+    Request,
+)
+from starlette.responses import (
+    Response,
+)
+
+from pddd.services import (
+    Service,
+    CreateService,
+    ReadService,
+    UpdateService,
+    DeleteService,
+    CrudService,
+)
+
+try:
+    from starlette_marshal import json
+except ImportError:
+    import json
+
+try:
+    from starlette_marshal import JSONResponse
+except ImportError:
+    from starlette.responses import JSONResponse
+
+
+class StarletteEndpoint(HTTPEndpoint):
+    service: Service
+
+
+class StarletteCreateMixin(object):
+    service: CreateService
+
+    async def post(self, request: Request) -> Response:
+        body: dict = json.loads(s=await request.body())
+        data: dict = await self.service.create(inputs=body)
+
+        return JSONResponse(content=data)
+
+
+class StarletteReadMixin(object):
+    service: ReadService
+
+    async def get(self, request: Request) -> Response:
+        query_params: dict = dict(request.query_params)
+        data: list = await self.service.read(filters=query_params)
+
+        return JSONResponse(content=data)
+
+
+class StarletteUpdateMixin(object):
+    service: UpdateService
+
+    async def patch(self, request: Request) -> Response:
+        body: dict = json.loads(s=await request.body())
+        id_: str = request.path_params.get("id")
+        data: dict = await self.service.update(id_=id_, inputs=body)
+
+        return JSONResponse(content=data)
+
+
+class StarletteDeleteMixin(object):
+    service: DeleteService
+
+    async def delete(self, request: Request) -> Response:
+        id_: str = request.path_params.get("id")
+        await self.service.delete(id_=id_)
+
+        return Response(status_code=204)
+
+
+class StarletteCrudEndpoint(
+    StarletteEndpoint,
+    StarletteCreateMixin,
+    StarletteReadMixin,
+    StarletteUpdateMixin,
+    StarletteDeleteMixin,
+):
+    service: CrudService
